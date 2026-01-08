2ca9d70e86992a94c4a8bbf3b0c07d8fc05ef4a8
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-08 22:00:06 +0530

    Add files via upload

---
+++ Array problem_16.py
@@ -0,0 +1,21 @@
+def longest_subarray_with_sum_k(arr, k):
+    prefix_sum = 0
+    seen = {}
+    max_len = 0
+
+    for i, num in enumerate(arr):
+        prefix_sum += num
+
+        if prefix_sum == k:
+            max_len = i + 1
+
+        if prefix_sum - k in seen:
+            max_len = max(max_len, i - seen[prefix_sum - k])
+
+        if prefix_sum not in seen:
+            seen[prefix_sum] = i
+
+    return max_len
+
+# Example
+print(longest_subarray_with_sum_k([1, 2, 3, 1, 1, 1, 5], 5))
