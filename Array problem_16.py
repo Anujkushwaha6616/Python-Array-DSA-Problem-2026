1a85ef188468ff63be8fd123e1487c15d7609d03
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-08 22:01:37 +0530

    Add problem description and example to code
    Added problem statement and example for clarity.

--- Array problem_16.py
+++ Array problem_16.py
@@ -1,3 +1,15 @@
+'''
+Problem
+
+Given an array and a value K, find the length of the longest subarray whose sum equals K.
+
+Example
+
+Input:
+arr = [1, 2, 3, 1, 1, 1, 5], K = 5
+Output:
+3
+'''
 def longest_subarray_with_sum_k(arr, k):
     prefix_sum = 0
     seen = {}
@@ -19,3 +31,4 @@ def longest_subarray_with_sum_k(arr, k):
 
 # Example
 print(longest_subarray_with_sum_k([1, 2, 3, 1, 1, 1, 5], 5))
+
