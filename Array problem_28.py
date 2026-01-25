de5102602f90c0ebce8f6fedc193923bedd123d7
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-25 19:53:28 +0530

    Add files via upload

---
+++ Array problem_28.py
@@ -0,0 +1,18 @@
+'''
+'''
+def longest_increasing_subarray(arr):
+    max_len = 1
+    curr_len = 1
+
+    for i in range(1, len(arr)):
+        if arr[i] > arr[i - 1]:
+            curr_len += 1
+            max_len = max(max_len, curr_len)
+        else:
+            curr_len = 1
+
+    return max_len
+
+
+arr = [1, 2, 2, 3, 4, 5]
+print(longest_increasing_subarray(arr))
