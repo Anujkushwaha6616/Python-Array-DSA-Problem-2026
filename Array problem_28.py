27a3602279fb88450525f7e635f4fb81944b32cc
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-25 19:56:47 +0530

    Add problem statement and example to the script
    Added problem description and example for clarity.

--- Array problem_28.py
+++ Array problem_28.py
@@ -1,4 +1,12 @@
 '''
+Problem: 
+
+Given an array of integers, find the length of the longest continuous subarray where each element is strictly greater than the previous one.
+
+Example
+Input:  [1, 2, 2, 3, 4, 5]
+Output: 4
+
 '''
 def longest_increasing_subarray(arr):
     max_len = 1
@@ -16,3 +24,4 @@ def longest_increasing_subarray(arr):
 
 arr = [1, 2, 2, 3, 4, 5]
 print(longest_increasing_subarray(arr))
+
